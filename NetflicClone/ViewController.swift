0ded6c1af10c72baf1b1f66be6cec8e00412c767
talhaPolat25 <easynick25@gmail.com>
2023-04-27 13:05:42 +0300

    Initial Commit

---
+++ NetflicClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflicClone
+//
+//  Created by talha polat on 27.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
