fb220a181b22c380afc5cd6222c2f431bd6a1592
talhaPolat25 <easynick25@gmail.com>
2023-07-11 14:39:45 +0300

    last update

--- NetflixClone/Manager/ApiManager.swift
+++ NetflixClone/Manager/ApiManager.swift
@@ -9,18 +9,17 @@ import Foundation
 import Alamofire
 struct Constants{
     
-    //static let baseUrl:String = "https://api.themoviedb.org"
-   // static let apiKey:String = "4f2ca9826ec85189db31539ace5eb27d"
-    static let trendingMoviesUrl = "https://api.themoviedb.org/3/trending/all/day?api_key=4f2ca9826ec85189db31539ace5eb27d"
-    static let popularMoviesUrl = "https://api.themoviedb.org/3/movie/popular?api_key=4f2ca9826ec85189db31539ace5eb27d&language=en-US&page=1"
-    static let upComingMovieUrl = "https://api.themoviedb.org/3/movie/upcoming?api_key=4f2ca9826ec85189db31539ace5eb27d&language=en-US&page=1"
-    static let trendingTvUrl = "https://api.themoviedb.org/3/trending/tv/week?api_key=4f2ca9826ec85189db31539ace5eb27d"
-    static let topRated = "https://api.themoviedb.org/3/movie/top_rated?api_key=4f2ca9826ec85189db31539ace5eb27d&language=en-US&page=1"
-    static let discoverMovies = "https://api.themoviedb.org/3/discover/movie?api_key=4f2ca9826ec85189db31539ace5eb27d&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate"
+ 
+    static let trendingMoviesUrl = "https://api.themoviedb.org/3/trending/all/day?api_key="
+    static let popularMoviesUrl = "https://api.themoviedb.org/3/movie/popular?api_key=d&language=en-US&page=1"
+    static let upComingMovieUrl = "https://api.themoviedb.org/3/movie/upcoming?api_key=d&language=en-US&page=1"
+    static let trendingTvUrl = "https://api.themoviedb.org/3/trending/tv/week?api_key="
+    static let topRated = "https://api.themoviedb.org/3/movie/top_rated?api_key=d&language=en-US&page=1"
+    static let discoverMovies = "https://api.themoviedb.org/3/discover/movie?api_key=d&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate"
     static let imageFetchBaseUrl = "https://image.tmdb.org/t/p/w500"
-    static let searchBaseUrl = "https://api.themoviedb.org/3/search/movie?api_key=\(Constants.apiKey)&query="
+    static let searchBaseUrl = "https://api.themoviedb.org/3/search/movie?api_key=&query="
     static let youtubeBaseURL = "https://youtube.googleapis.com/youtube/v3/search?q="
-    static let youtubeApiKey = "AIzaSyB06BMVBImzvfC5SI8fn8UPM_0EN4PV7VE"
+    static let youtubeApiKey = ""
     
 }
 enum ApiErrors:String,Error{
@@ -76,7 +75,7 @@ class ApiManager{
         let query = query.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)
         
         guard let url = URL(string: "\(Constants.youtubeBaseURL)\(query!)&key=\(Constants.youtubeApiKey)")else{return}
-        print("Baba hele bakisannn \n \(url)")
+        
         let request = URLRequest(url: url)
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
             guard let data = data else{return}
